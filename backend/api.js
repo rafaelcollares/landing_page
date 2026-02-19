84c32c7c1dbfb31e147df500c08b7dd6c924bf9c
= <=>
2026-02-19 17:55:50 -0300

    fix smtp

--- backend/api.js
+++ backend/api.js
@@ -2,7 +2,9 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
