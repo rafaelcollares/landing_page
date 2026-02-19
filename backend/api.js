977078c3bca4fd0582058f8116949abc5560b174
= <=>
2026-02-19 17:11:13 -0300

    fix error 500

--- backend/api.js
+++ backend/api.js
@@ -2,14 +2,13 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
-  }
+  },
 });
+
 
 // Email que chega para você
 async function sendMail(req, res) {
